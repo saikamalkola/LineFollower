8bb5c15241334ffa9356954b2c0d7badc3be87bd
saikamalkola <saikamalkola@gmail.com>
2017-01-07 13:49:49 +0530

    Add files via upload

--- linefollower_PID/linefollower_PID.ino
+++ linefollower_PID/linefollower_PID.ino
@@ -1,265 +1,251 @@
-int l1=2;
-int l2=3;
-//left motor pins
-int r1=4;
-int r2=5;
-//right motor pins
-int enl=A4;
-int enr=A5;
-//motor enabler pins
+int l1=5;
+int l2=6;
+
+int r1=10;
+int r2=9;
+
 int a[6];
-//sensor array
 int last_proportional=0;
 int integral=0;
+#define THRESHOLD 560
 
-int mod(int v);
-//modulus function
-int setmotors(int a,int b);
+
+//bool leds=false;
+
+
+char select_turn(unsigned char found_left,unsigned char found_right,unsigned char found_st); //AI INITIALIZAION
+
+
+int set_motors(int a,int b);
 void turn(char dir);
-int PID();
-int right=0;
-int left=0;
+void PID();
 
-int systrack=0;
-  int i=0;
-  int turn=0;
-  char exparr[200];
-  int count=0;
-  int IR_above=0;
+ int right=0;
+ int left=0;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(l1,OUTPUT);
-  pinMode(l2,OUTPUT);
 
-  pinMode(r1,OUTPUT);
+  pinMode(l1,OUTPUT);   // Left motor output
+  pinMode(l2,OUTPUT);
+  
+  pinMode(r1,OUTPUT);  //Right motor output
   pinMode(r2,OUTPUT);
 
-  pinMode(enl,OUTPUT);
-  pinMode(enr,OUTPUT);
 
-  pinMode(patchledL,OUTPUT);
-  pinMode(patchledR,OUTPUT);
-  pinMode(binary,OUTPUT);
-  pinMode(octal,OUTPUT);
-  
   Serial.begin(9600);
+
+ // pinMode(ledg,OUTPUT);
+  //pinMode(ledr,OUTPUT);
+//  digitalWrite(ledg,leds);
+ // digitalWrite(ledr,leds);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int k=PID();
-  if(k!=0)
-  {
-  set_motors(150,150);
-  delay(20);
-  unsigned char found_left =0;
-  unsigned char found_right=0;
+  
+
+PID();    //Control invoked
+ 
+  unsigned char found_left=0;
+  unsigned char found_right=0;   // turn paratmeter.
   unsigned char found_st=0;
 
-  readline();
-  
-  if(a[0]==LOW&&a[3]!=LOW)
-  {
-  found_left=1;
-  found_right=0;
-  turn=turn+1;
-  }
-
-  if(a[3]==LOW&&a[0]!=LOW)
-  {
-  found_right=1;
-  found_left=0;
-  turn=turn+1;
-  }
-  if(a[1]==LOW || a[2]==LOW)
-  found_st=1
-  
-  if(turn%2==0)
-  {
-    systrack=0;
-    digitalWrite(binary,HIGH);
-    digitalWrite(octal,LOW);
-  }
-  else 
-  {
-    systrack=1;
-    digitalWrite(octal,HIGH);
-    digitalWrite(binary,LOW);
-  }
+readline();
+  if(a[0]==LOW)
+    found_left=1;
+ 
+  if(a[1]==HIGH&&(a[2]==LOW||a[3]==LOW)&&a[4]==HIGH)
+  found_st=1;
+     
+  if(a[5]==LOW)
+    found_right=1;
+ 
 
   unsigned char dir;
-
-  dir=select_turn(found_left,found_right,found_st)
+  Serial.print(" ");
+  Serial.print(found_left);
+  Serial.print(" ");
+  Serial.print(found_right);
+  Serial.print(" ");
+  Serial.print(found_st);
+  Serial.print(" ");
+  dir=select_turn(found_left, found_right, found_st);
+  
   turn(dir);
-}
-}
-else
-{
-  set_motors(0,0);
-  //evaluate expression
-}
-}
-int set_motors(int l,int r)
-{
-  Serial.println(l);
-  Serial.println(r);
 
- if(l>0 && r>0)
- {
- analogWrite(enl,mod(l));
- analogWrite(enr,mod(r));
- digitalWrite(l1,LOW);
- digitalWrite(l2,HIGH);
- digitalWrite(r1,LOW);
- digitalWrite(r2,HIGH); 
- }
- else if(l<0 && r>0)
- {
- analogWrite(enl,mod(l));
- analogWrite(enr,mod(r));
- digitalWrite(l1,HIGH);
- digitalWrite(l2,LOW);
- digitalWrite(r1,LOW);
- digitalWrite(r2,HIGH); 
- }
- else if(l>0 && r<0)
- {
- analogWrite(enl,mod(l));
- analogWrite(enr,mod(r));
- digitalWrite(l1,LOW);
- digitalWrite(l2,HIGH);
- digitalWrite(r1,HIGH);
- digitalWrite(r2,LOW); 
- }
- else if(l==0 && r==0)
- {
- analogWrite(enl,mod(l));
- analogWrite(enr,mod(r));
- digitalWrite(l1,HIGH);
- digitalWrite(l2,HIGH);
- digitalWrite(r1,HIGH);
- digitalWrite(r2,HIGH); 
- }
- 
-}
-void turn(char dir)
+
+
+
+
+}// MAIN body ends
+
+
+
+int set_motors(int l,int r) // Motor setup
 {
+
+
+  Serial.print(l);
+  Serial.print(" ");
+  Serial.println(r);
+ 
+if(l>0&&r>0)
+{
+ 
+  analogWrite(l1,0);
+  analogWrite(l2,l);
+    analogWrite(r1,0);
+  analogWrite(r2,r);
+    
+  
+}
+
+else if(l<0&&r>0)
+{
+  analogWrite(l1,-l);
+  analogWrite(l2,0);
+    analogWrite(r1,0);
+  analogWrite(r2,r);
+  
+}
+
+else if(l>0&&r<0)
+{analogWrite(l1,0);
+  analogWrite(l2,l);
+    analogWrite(r1,-r);
+  analogWrite(r2,0);
+}
+
+else if(l==0&&r==0)
+{
+ 
+
+  analogWrite(l1,l);
+  analogWrite(l2,l);
+    analogWrite(r1,r);
+  analogWrite(r2,r);
+}
+}
+
+int readline()
+
+{a[0]=digitalRead(4);
+Serial.print(a[0]);
+Serial.print(" ");
+  for(int i=4;i>=0;i--) // Read line Function
+  {
+    if(analogRead(A0+i)<=THRESHOLD)
+    a[5-i]=LOW;
+    else
+    a[5-i]=HIGH;
+    Serial.print(a[5-i]);
+    Serial.print("  ");
+  }
+
+  int v;
+ v=(5000*a[0]+4000*a[1]+3000*a[2]+2000*a[3]+1000*a[4]+0*a[5])/(a[0]+a[1]+a[2]+a[3]+a[4]+a[5]);
+ 
+ return v;
+}
+
+int STOP()
+{
+  analogWrite(l1,255);
+  analogWrite(l2,255);
+    analogWrite(r1,255);
+  analogWrite(r2,255);
+  
+}
+
+void turn(char dir)   //Turning setup
+{set_motors(0,0);
+  delay(5);
   switch(dir)
   {
     case 'L':
-    set_motors(-200,200);
-    delay(350);
-    break;
+      while(1){
+      readline();
+      set_motors(-50,50);
+      if((a[2]==LOW&&a[3]==LOW))
+      break;}
+      break;
     case 'R':
-    set_motors(200,-200);
-    delay(350);
-    break;
+      while(1){
+      readline();
+      set_motors(50,-50);
+      if((a[2]==LOW&&a[3]==LOW))
+      break;}
+      break;
     case 'B':
-    set_motors(200,-200);
-    delay(650);
-    break;
+      while(1){
+      readline();
+      set_motors(50,-50);
+      if((a[2]==LOW&&a[3]==LOW))
+      break;}
+      break;
     case 'S':
     break;
   }
-}
-int PID()
-{
-  int i;
-  int error=0;
+    
+ }
+
+
+void PID()
+{ int i;       // Control function
+  int power_difference=0;
   float Kp,Ki,Kd;
   unsigned int position;
   int derivative,proportional;
   while(1)
-  {
-   position=readline();
-   if(IR_above==HIGH)
-   {
-   Serial.println(position);
-   proportional=int(position-1500)
-   derivative=proportional-lastproportional;
-   integral=integral+proportional;
-   last_proportional=proportional;
-   Kp=0.09;
-   Ki=0.0001;
-   Kd=1.0;
-   error=Kp*proportional+Ki*integral+Kd*derivative;
-   const int max=180;
-   if(error>max)
-   error=max;
-   if(erroe<-1*max)
-   error=-1*max;
-   if(error<0)
-   set_motors(max+error,max);
-   else
-   set_motors(max,max-error);
-    if(a[0]==HIGH&&a[5]==LOW)
-   {
-    if(count%2==0)
-    {if(systrack==0)
-    exparr[count]='1';
-    else exparr[count]='1';
-    }
-    else
-    {
-      if(found_left==0&&found_right==0)
-      exparr[count]='+';
-      else if(found_left==1&&found_right==0)
-      exparr[count]='+';
-      else if(found_left==0&&found_right==1)
-      exparr[count]='*';
-    }
-    count=count+1;
-    digitalWrite(patchledR,HIGH);
-  }
-  else if(a[0]==HIGH&&a[5]==LOW)
   { 
-    if(count%2==0)
-    {if(systrack==0)
-    exparr[count]='2';
-    else exparr[count]='8';
-    }
-    else
-    {
-      if(found_left==0&&found_right==0)
-      exparr[count]='-';
-      else if(found_left==1&&found_right==0)
-      exparr[i]='-';
-      else if(found_left==0&&found_left==1)
-      exparr[i]='/';
-    couont=count+1;
-    digitalWrite(patchledL,HIGH);
-  }
-  }
-   readline();
-   if(a[0]==HIGH&&a[1]==HIGH&&a[2]==HIGH&&a[3]==HIGH)
-   return 1;
-   else if(a[0]==LOW || a[3]==LOW)
-   return 1;
+  position=readline();
+  
+  proportional=((int)position-2500);
+  
+   derivative = proportional-last_proportional;
+   integral = integral+proportional;
+
+  last_proportional=proportional;
+  Kp=0.1;
+  Ki=0.0001;
+  Kd=1.0;
+
+  power_difference= proportional*Kp + integral*Ki + derivative*Kd;
+  const int max=120;
+  if(power_difference>max)
+   power_difference=max;
+  if(power_difference<-max)
+   power_difference= (-1*max);
+   
+  if(power_difference<0)
+  {//left
+   set_motors(max+power_difference,max);
   }
   else
-  return 0;
+  {
+   set_motors(max,max-power_difference); //right
+  }
+  readline();
+  if(a[0]==HIGH&&a[1]==HIGH&&a[2]==HIGH&&a[3]==HIGH&&a[4]==HIGH&&a[5]==HIGH)
+   STOP();
+  else if(a[0]==LOW||a[5]==LOW)
+    return;
+  }  
 }
 
-int readline()
-{ int pos=0,tot=0;
-  for(int i=0;i<6;i++)
-  {
-    a[i]=digitalRead(i+2);
-  }
-  IR_above=digitalRead(8);
-  pos=(3000*a[1]+2000*a[2]+1000*a[3]+0*a[4])/a[1]+a[2]+a[3]+a[4];
-  return pos;  
-}
-char select_turn(unsigned char found_left,unsigned char found_right,unsigned char found_st)
+char select_turn(unsigned char found_left,unsigned char found_right,unsigned char found_st) // AI Function
 {
-  if(found_left==1)
-  return 'L';
-  else if(found_st==1)
-  return 'S';
-  else if(found_right==1)
-  return 'R';
-  else
-  return 'B';
+ if(found_left==1)
+ return 'L';
+ else if(found_st==1)
+ return 'S';
+ else if(found_right==1)
+ return 'R';
+ else
+ return 'B';
 }
+
+
+
+
+
 
